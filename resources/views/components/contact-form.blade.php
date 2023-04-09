7a7a486a73c348134f5928abe9e327fa80a82c73
mohammad <mohammad_jahromi_73@yahoo.com>
2023-04-09 16:53:08 +0430

    fix-jquary-form

--- resources/views/components/contact-form.blade.php
+++ resources/views/components/contact-form.blade.php
@@ -10,19 +10,23 @@
                                 <label class="form-label">Your Name <span class="text-danger">*</span></label>
                                 <div class="form-icon position-relative">
                                     <i data-feather="user" class="fea icon-sm icons"></i>
-                                    <input name="name" id="name" type="text" class="form-control ps-5" placeholder="First Name :">
+                                    <input name="name" id="name" type="text" class="form-control ps-5"
+                                        placeholder="First Name :">
                                 </div>
                             </div>
-                        </div><!--end col-->
+                        </div>
+                        <!--end col-->
                         <div class="col-lg-6">
                             <div class="mb-3">
                                 <label class="form-label">Your Phone <span class="text-danger">*</span></label>
                                 <div class="form-icon position-relative">
-                                    <i data-feather="mail" class="fea icon-sm icons"></i>
-                                    <input name="email" id="email2" type="email" class="form-control ps-5" placeholder="Your email :">
+                                    <i data-feather="phone" class="fea icon-sm icons"></i>
+                                    <input name="phone" id="phone" type="number" class="form-control ps-5"
+                                        placeholder="Your phone :">
                                 </div>
-                            </div> 
-                        </div><!--end col-->
+                            </div>
+                        </div>
+                        <!--end col-->
                         <div class="col-lg-12">
                             <div class="mb-3">
                                 <label class="form-label">Comments</label>
@@ -32,17 +36,54 @@
                                 </div>
                             </div>
                         </div>
-                    </div><!--end row-->
+                    </div>
+                    <!--end row-->
                     <div class="row">
                         <div class="col-sm-12 text-center">
                             <div class="d-grid">
-                                <input type="submit" id="submit" name="send" class="submitBnt btn btn-primary" value="Send Message">
+                                <input type="submit" id="submit" name="send" class="submitBnt btn btn-primary"
+                                    value="Send Message">
                                 <div id="simple-msg"></div>
                             </div>
-                        </div><!--end col-->
-                    </div><!--end row-->
-                </form><!--end form--> 
-            </div><!--end custom-form-->
+                        </div>
+                        <!--end col-->
+                    </div>
+                    <!--end row-->
+                </form>
+                <!--end form-->
+            </div>
+            <!--end custom-form-->
         </div>
     </div>
-</div><!--end col-->
+</div>
+<!--end col-->
+
+<script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $("#submit").click(function(event) {
+            event.preventDefault();
+            // alert('okkkkk');
+
+            // get Data
+            var name = $('#name').val();
+            var phone = $('#phone').val();
+            var comments = $('#comments').val();
+
+            console.log("name: " + name + " phone: " + phone + " comments: " + comments);
+            
+            $.ajax({
+                url: {{route('comments')}},
+                type: 'POST',
+                data: {'name': name, 'phone': phone, 'comments': comments},
+                success: function(response) {
+                    alert(response.status);
+                },
+                error: function() {
+                    alert("error");
+                }
+            });
+        });
+    });
+</script>
